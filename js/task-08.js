33be390fb5042a30bb50c79c841472eea787d7da
KamilaWaniek <k.scheffera@wp.pl>
2024-01-31 14:45:33 +0100

    Update task-08.js

---
+++ js/task-08.js
@@ -0,0 +1,21 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  //Jeśli pole email i password są puste, zwraca specjalny alert
+  if (emailInput.value === "" || passwordInput.value === "") {
+    alert("All fields should be fill in");
+  } else {
+    const dataForm = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+    consoleLog(dataForm);
+
+    //wymazuję wprowadzoną treść w polach poprzez reset
+    loginForm.reset();
+  }
+});
