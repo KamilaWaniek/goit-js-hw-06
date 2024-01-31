1bdf1460cd18d1e7fd5436b8408cce6bed08df04
KamilaWaniek <k.scheffera@wp.pl>
2024-01-31 13:45:50 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,5 +1,3 @@
-document.body.onload = addElement;
-
 const ingredients = [
   "Potatoes",
   "Mushrooms",
@@ -9,16 +7,20 @@ const ingredients = [
   "Condiments",
 ];
 
-const newIngredient = ingredient.createElement("li");
-const newContent = ingredient.createTextNode("salt");
-newIngredient.appendChild(newContent);
-
-const currentLi = document.getElementById("li1");
-document.body.insertBefore(newIngredient, currentLi);
-
-//Napisz skrypt, który dla każdego elementu tablicy ingredients:
+const ulElements = document.querySelector("#ingredients");
 
 //Utworzy oddzielny element <li>. Obowiązkowo użyj metody document.createElement().
-//Doda nazwę elementu (string z tablicy) jako jego zawartość tekstową.
-//Doda do elementu klasę item.
-//Następnie jedna operacja umieści wszystkie <li> na liście ul#ingredients.
+
+ingredients.forEach((ingredient) => {
+  const newIngredient = document.createElement("li");
+
+  //Doda nazwę elementu (string z tablicy) jako jego zawartość tekstową.
+  newIngredient.textContent = ingredient;
+
+  //Doda do elementu klasę item.
+  newIngredient.classList.add("item");
+
+  //Następnie jedna operacja umieści wszystkie <li> na liście ul#ingredients.
+  ulElements.append(newIngredient);
+});
+console.log(ulElements);
